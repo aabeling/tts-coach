3072dad43a574d4fc096df5baf0a5f325142c1d8
Achim Abeling <achimabeling@web.de>
2020-12-17 12:17:02 +0100

    weitere Übungen und Refactorings

--- staerkung_lendenwirbelsaeule.sh
+++ staerkung_lendenwirbelsaeule.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PAUSE_AFTER_EXPLANATION=5
+PAUSE_AFTER_EXPLANATION=2
 EXERCISE_DURATION=10
 PAUSE_AFTER_EXERCISE=5
 
@@ -32,104 +32,159 @@ function explainExercise() {
     say "Es geht los in drei, zwei, eins, los!"
 }
 
+function entspannen() {
+
+  # mary spricht entspannen ansonsten mit einem spitzen S aus
+  say "enschpannen"
+  sleep $PAUSE_AFTER_EXERCISE
+}
+
 function uebung1() {
   explainExercise "Übung 1: Entspannung" "Füße mit leichtem Druck gegen die Wand und Wirbelsäule gegen den Boden pressen, danach Spannung wieder abbauen."
 
-  say "Wiederholung 1"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
-
-  say "Wiederholung 2"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
-
-  say "Wiederholung 3"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
+  for i in $(seq 1 3); do
+    say "Wiederholung $i. Pressen!"
+    sleep $EXERCISE_DURATION
+    entspannen
+  done
 }
 
 function uebung2() {
   explainExercise "Übung 2: Grundspannung Rückenlage" "Fußspitzen anziehen, Fersen auf den Boden drücken, Bauch- und Gesäßmuskulatur anspannen und Lendenwirbelsäule auf den Boden pressen."
 
-  say "Wiederholung 1"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
-
-  say "Wiederholung 2"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
-
-  say "Wiederholung 3"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
+  for i in $(seq 1 3); do
+    say "Wiederholung $i"
+    sleep $EXERCISE_DURATION
+    entspannen
+  done
 }
 
 function uebung3() {
   explainExercise "Übung 3: Rückenlage" "Kopf und Schulter anheben, dann Arme leicht anheben und gegen einen gedachten Widerstand drücken"
 
-  say "Wiederholung 1"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
-
-  say "Wiederholung 2"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
-
-  say "Wiederholung 3"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
+  for i in $(seq 1 3); do
+    say "Wiederholung $i"
+    sleep $EXERCISE_DURATION
+    entspannen
+  done
 }
 
 function uebung4() {
+
   explainExercise "Übung 4: Grundspannung Rückenlage" "Beine anwinkeln. Mit der rechten Hand das linke Bein berühren und zehn Sekunden halten. Danach wechseln."
 
-  say "Wiederholung 1: rechte Hand an linkes Bein"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
+  for i in $(seq 1 3); do
+    say "Wiederholung $i: rechte Hand an linkes Bein"
+    sleep $EXERCISE_DURATION
+    entspannen
 
-  say "linke Hand an rechtes Bein"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
+    say "und Seite wechseln: linke Hand an rechtes Bein"
+    sleep $EXERCISE_DURATION
+    entspannen
+  done
+}
 
-  say "Wiederholung 2: rechte Hand an linkes Bein"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
+function uebung5() {
 
-  say "linke Hand an rechtes Bein"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
+  explainExercise "Übung 5: Rückenlage" "Linken Arm und rechtes Bein auf den Boden drücken. Mit der rechten Hand gegen das linke Bein drücken. Danach Seitenwechsel."
 
-  say "Wiederholung 3: rechte Hand an linkes Bein"
-  sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
+  for i in $(seq 1 3); do
+    say "Wiederholung $i: mit der rechten Hand gegen das linke Bein drücken"
+    sleep $EXERCISE_DURATION
+    say "und Seite wechseln: mit der linken Hand gegen das rechte Bein drücken"
+    sleep $EXERCISE_DURATION
+  done
+}
 
-  say "linke Hand an rechtes Bein"
+function uebung6() {
+
+  explainExercise "Übung 6: Rückenlage" "Gesäß und Wirbelsäule anheben und Bauch und Gesäßmuskulatur anspannen."
+
+  for i in $(seq 1 3); do
+    say "Wiederholung $i: hoch mit dir, wird's bald!"
+    sleep $EXERCISE_DURATION
+    entspannen
+  done
+}
+
+function uebung7() {
+
+  explainExercise "Übung 7: Rückenlage" "Ein Knie mit beiden Händen umfassen und zur Brust ziehen. Dann Arme strecken und Knie gegen den Händewiderstand drücken."
+
+  say "Mit dem linken Bein"
   sleep $EXERCISE_DURATION
-  say "Entspannen"
-  sleep $PAUSE_AFTER_EXERCISE
+
+  say "Mit dem rechten Bein"
+  sleep $EXERCISE_DURATION
+}
+
+function uebung8() {
+
+  explainExercise "Übung 8: Grundspannung Bauchlage" "Fersen wegdrücken, Oberkörper leicht anheben. Hals nicht überstrecken."
+
+  for i in $(seq 1 3); do
+    say "Wiederholung $i: hoch mit dir, fauler Hund!"
+    sleep $EXERCISE_DURATION
+    entspannen
+  done
+}
+
+function uebung9() {
+
+  explainExercise "Übung 9: Bauchlage" "Arme in U-Form anheben und die Schulterblätter Richtung Wirbelsäule zusammenschieben."
+
+  for i in $(seq 1 3); do
+    say "Wiederholung $i: hoch mit dir, Lusche!"
+    sleep $EXERCISE_DURATION
+    entspannen    
+  done
+}
+
+function uebung10() {
+
+  explainExercise "Übung 10: Bauchlage" "Arme und Oberkörper leicht anheben und nach beiden Seiten leicht verlagern. Hals nicht überstrecken."
+
+  say "und hoch für 30 Sekunden"
+  sleep 30
+}
+
+function uebung11() {
+
+  explainExercise "Übung 11: Vierfüßlerstand" "Beine und Arme kreuzweise in der Waagerechten halten."
+
+  for i in $(seq 1 3); do
+    say "Wiederholung $i. Rechter Arm und linkes Bein"
+    sleep $EXERCISE_DURATION
+    say "und die Seite wechseln"
+    sleep $EXERCISE_DURATION
+  done
+}
+
+function uebung12() {
+
+  explainExercise "Übung 12: Vierfüßlerstand" "Katzenbuckel bilden"
+
+  for i in $(seq 1 3); do
+    say "Wiederholung $i: buckel dich!"
+    sleep $EXERCISE_DURATION
+    entspannen
+  done
 }
 
 startMaryTts
 
-say "Rückentraining für die Lendenwirbelsäule"
-sleep 1
-say "bitte fertig machen"
+# mary spricht träning besser aus als training!
+say "Rückenträning für die Lendenwirbelsäule"
 
 uebung1
 uebung2
 uebung3
 uebung4
+uebung5
+uebung6
+uebung7
+uebung8
+uebung9
+uebung10
+uebung11
+uebung12
